b56fdcab63e5b863092229bc0fea7f18ce2b7d81
Borizina <151845726+Borizina@users.noreply.github.com>
2024-07-10 11:33:43 -0800

    second commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'eact';
+import { View, Text } from 'eact-native';
+import DrawerNavigator from './DrawerNavigator';
+
+const App = () => {
+  return (
+    <DrawerNavigator />
+  );
+};
+
+export default App;
